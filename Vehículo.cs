eda62aa7fec9aeade9c9eb23fbd4c830571843a6
JuanMacias07x <jmaciascaldern07@gmail.com>
2021-09-16 11:55:38 -0500

    1

---
+++ Vehículo.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiagramaClasesUML
+{
+    public class Vehículo
+    {
+        private string marca;
+        private string línea;
+        private string color;
+        private int modelo;
+        private int placa;
+        private string cilindraje;
+        private string ClaseVehículo;
+        private int potencia;
+        private int capacidad;
+        /// <summary>
+        /// Posee o no blindaje
+        /// </summary>
+        private bool blindado;
+        private string carrocería;
+        private string combustible;
+        private string TipoServicio;
+
+        private int Blindado
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Capacidad
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Carrocería
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Clase
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Color
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Combustible
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Línea
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Marca
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Modelo
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Placa
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Potencia
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private int Tipo
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
